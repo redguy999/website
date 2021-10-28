6c93e18a3c49e04b695e62e2014d60a60fc20f66
Patrick Buehlmann <Bigredguy05@hotmail.com>
2021-10-28 14:01:41 -0400

    adds subcomment

--- roguelike/equipConstructors.js
+++ roguelike/equipConstructors.js
@@ -53,7 +53,7 @@ const enemies = [
     {name:"Goblin",Mhealth:"10",attack:"1",defense:"0",color:"orange"},//color is only for displaying where the enemy is.
     {name:"Armored goblin",Mhealth:"15",attack:"2",defense:"3",color:"grey"},
 ];
-//{name:"None",Mhealth:"0",attack:"0",defense:"0",color:"orange"},
+//{name:"None",Mhealth:"0",attack:"0",defense:"0",color:"orange"}, //this is the framework
 class equiped{
     constructor(name="missingN",attack=0,defense=0,healthUp=0,special=null){//currently no way to read special.
         this.name = name;
