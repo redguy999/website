65fcbd8bd5ac96d5307f0290bd0c835ff62a235b
redguy999 <Bigredguy05@hotmail.com>
2021-12-07 08:04:59 -0500

    adds some more code ideas.

--- roguelike/codeIdeas/randomCode.js
+++ roguelike/codeIdeas/randomCode.js
@@ -1,14 +1,15 @@
-const grid={}//in theory could be an array of arrays, but then the index values would all be off by one, which is a problem.
+const grid={}//in theory could be an array of arrays, but then the index values would all be off by one, or there would be an empty index, both of which would be a problem.
 let hT = 9//number of columns of tiles, AKA the max x value.
 let wT = 10	//number of rows of tiles, AKA the max y value.
 function mkGrid(){//makes the grid in the grid const above.
     for(x=1;x<=hT;x++){
         grid[x]={}
         for(y=1;y<=wT;y++){
-            grid[x][y]="empty"
+            grid[x][y]=""//empty string so falsy test gives false.
         }
     }
 }
+//corrdinate call format = "grid[x][y]"
 mkGrid()
 function setRow(row,arg){//runs a function over every tile in a given row.
     for(x in grid[row]){//notice, 'x' will be the index value of the tiles in the row.
@@ -23,7 +24,7 @@ function setColumn(column,arg){//runs a function over every tile in a given colu
         arg(x)
     }
 }
-setColumn(3,function(J){
-    grid[J][3]='door'
+setColumn(3,function(J){//setColumn test.
+    grid[J][3]='door'//set everything in column 3 to 'door'
 })
 console.log(grid[2]);
