b0ee0e15d271ced0ef055fbfd0801cccb87c8ecb
redguy999 <Bigredguy05@hotmail.com>
2022-02-03 08:06:03 -0500

    fixes some valididation links

---
+++ roguelike/newRogue/newConstructors.js
@@ -0,0 +1,5 @@
+const enemies = {//name is needed here, cause of how the indices work for objects.
+    "Goblin":{hp:"10",atk:"1",def:"0",color:"green",table:{"gold coin":[50,2],}},//color is only for displaying where the enemy is.
+    "Armored goblin":{hp:"15",atk:"2",def:"3",color:"darkolivegreen",table:{spear:[30,1],shield:[20,1],sword:[20,1],"gold coin":[50,5]}},
+    "Rock golem":{hp:"30",atk:"4",def:"4",color:"grey",table:{"rock flail":[50,1],"gold coin":[100,50]}},
+};
