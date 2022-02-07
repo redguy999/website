dc1cd2e0182f6b1b6911bdbf4e35549933da6834
redguy999 <Bigredguy05@hotmail.com>
2022-02-07 09:20:57 -0500

    auto formating

--- roguelike/newRogue/newConstructors.js
+++ roguelike/newRogue/newConstructors.js
@@ -1,26 +1,26 @@
 const enemies = {//name is needed here, cause of how the indices work for objects.
-    "Goblin":{hp:"10",atk:"1",def:"0",color:"green",/*table:{"gold coin":[50,2],}*/},//color is only for displaying where the enemy is.
-    "Armored goblin":{hp:"15",atk:"2",def:"3",color:"darkolivegreen",/*table:{spear:[30,1],shield:[20,1],sword:[20,1],"gold coin":[50,5]}*/},
-    "Rock golem":{hp:"30",atk:"4",def:"4",color:"grey",/*table:{"rock flail":[50,1],"gold coin":[100,50]}*/},
+    "Goblin": { hp: "10", atk: "1", def: "0", color: "green",/*table:{"gold coin":[50,2],}*/ },//color is only for displaying where the enemy is.
+    "Armored goblin": { hp: "15", atk: "2", def: "3", color: "darkolivegreen",/*table:{spear:[30,1],shield:[20,1],sword:[20,1],"gold coin":[50,5]}*/ },
+    "Rock golem": { hp: "30", atk: "4", def: "4", color: "grey",/*table:{"rock flail":[50,1],"gold coin":[100,50]}*/ },
 };
-function pickEnemy(){
+function pickEnemy() {
     let hold = []
-    for(x in enemies){//this will need modified for the floors
+    for (x in enemies) {//this will need modified for the floors
         hold.push(x);
     }
-    let temp = Math.floor(Math.random()*hold.length)//pick a number equal to or between 0 and the highed index number of the list.
+    let temp = Math.floor(Math.random() * hold.length)//pick a number equal to or between 0 and the highed index number of the list.
     return hold[temp]//returns the randomly picked enemy (specificly the name)
 }
-function placeEnemies(){
+function placeEnemies() {
     var href = getCorrdHref(ranArrCorrd())
     let name = pickEnemy()
     let eHold = enemies[name]
-    href.dis.style.backgroundColor=eHold.color;
-    href.content=new enemy(name,eHold.hp,eHold.atk,eHold.def,href)
+    href.dis.style.backgroundColor = eHold.color;
+    href.content = new enemy(name, eHold.hp, eHold.atk, eHold.def, href)
     //href.content=new 
 }
-class enemy{
-    constructor(name,Mhealth,attack,defense,ref,table=null){
+class enemy {
+    constructor(name, Mhealth, attack, defense, ref, table = null) {
         this.name = name;//string
         this.location = ref;//So it can delete itself when it needs to.
         this.Mhealth = Mhealth;//number
@@ -28,38 +28,38 @@ class enemy{
         this.attack = attack; //number
         this.defense = defense; //number
         this.table = table;//currently unused.
-        this.hurt = function(damg,retal=true){//retal is short for retaliate.
+        this.hurt = function (damg, retal = true) {//retal is short for retaliate.
             let temp = 0;
-            if(damg>0){//if damage is negative, ignore defense, since it is likely healing.
-            temp = Math.ceil(damg-(this.defense/2))//current defense calucation formula, will likely change it.
-            if(temp<=0){
-                TtC("The "+this.name+"'s armor completely protects them from harm!")
-                if(retal==true){this.fight();}//skip to attack
-                return;//nothing else to do.
-            }
-            } else{
+            if (damg > 0) {//if damage is negative, ignore defense, since it is likely healing.
+                temp = Math.ceil(damg - (this.defense / 2))//current defense calucation formula, will likely change it.
+                if (temp <= 0) {
+                    TtC("The " + this.name + "'s armor completely protects them from harm!")
+                    if (retal == true) { this.fight(); }//skip to attack
+                    return;//nothing else to do.
+                }
+            } else {
                 temp = damg
             }
             //probably some check for special attributes, oh and also defense
             //if the enemy doesn't have "health" which can be determined by checking max health, call a special function.
-            this.health-=temp;
-            if(temp>0){
-                TtC("The "+this.name+" takes "+temp+" damage!");
-            } else{
-                TtC("The "+this.name+" heals "+(-temp)+" health!");//TODO: reword this.
+            this.health -= temp;
+            if (temp > 0) {
+                TtC("The " + this.name + " takes " + temp + " damage!");
+            } else {
+                TtC("The " + this.name + " heals " + (-temp) + " health!");//TODO: reword this.
             }
-            if(this.health>this.Mhealth){//this function can be used for healing too.
-                this.health=this.Mhealth;
-            } else if(this.health<=0){
+            if (this.health > this.Mhealth) {//this function can be used for healing too.
+                this.health = this.Mhealth;
+            } else if (this.health <= 0) {
                 this.dead()
                 return;//need to early return because otherwise attack runs.
             }
-            if(retal==true){this.fight();}
+            if (retal == true) { this.fight(); }
         }
-        this.dead = function(){
-            TtC(this.name+" has been defeated.");
-            this.location.dis.style.backgroundColor="";
-            if(table){//hasn't been ported yet.
+        this.dead = function () {
+            TtC(this.name + " has been defeated.");
+            this.location.dis.style.backgroundColor = "";
+            if (table) {//hasn't been ported yet.
                 // let temp = dropLoot(this.table);
                 // if(temp===null){//drop nothing
 
@@ -76,22 +76,22 @@ class enemy{
             }
             removeEnemy(this.location)
         }
-        this.fight = function(){//might make it able to attack anyone.
-            if(this.attack==0){
+        this.fight = function () {//might make it able to attack anyone.
+            if (this.attack == 0) {
                 return;//early exit in case the enemy can't attack.
             }
-                player.hurtPlayer(this.attack);
+            player.hurtPlayer(this.attack);
         }
     }
 }
-function removeEnemy(loc){
-    loc.content="";
+function removeEnemy(loc) {
+    loc.content = "";
 }
 //above is enemy code, the rest is below
-function mkOtherStuff(){
+function mkOtherStuff() {
     //placeEnemies();
     placeItems()
 }
-function placeItems(){
+function placeItems() {
     let href = getCorrdHref(ranArrCorrd())
 }
--- roguelike/newRogue/overhualed.js
+++ roguelike/newRogue/overhualed.js
@@ -1,107 +1,107 @@
-const dGrid=document.getElementById("displayGrid")
-const aGrid=document.getElementById("actionGrid")
-const gridObj={}//While this could be an array, the 0th index would mess everything up.
+const dGrid = document.getElementById("displayGrid")
+const aGrid = document.getElementById("actionGrid")
+const gridObj = {}//While this could be an array, the 0th index would mess everything up.
 //might remove gridObj
-const rows=9
-const columns=8
-var playerLoc="1,1"
+const rows = 9
+const columns = 8
+var playerLoc = "1,1"
 start()
-function start(){
+function start() {
     mkGrid();
     mkWalls();
     setStart();
     setExit();
     resetLocDisplay()
 }
-function nextLevel(){
+function nextLevel() {
     clearGrid();
     mkWalls();
     setStart();
     setExit();
     resetLocDisplay();
 }
-function clearGrid(){
-    for(let x in gridObj){
-        for(let y in gridObj[x]){
-            gridObj[x][y].dis.style.backgroundColor="";//clears the background color
-            gridObj[x][y].content=""
+function clearGrid() {
+    for (let x in gridObj) {
+        for (let y in gridObj[x]) {
+            gridObj[x][y].dis.style.backgroundColor = "";//clears the background color
+            gridObj[x][y].content = ""
             //act isn't really used by the code currently.
             //loc is effectively a constant, setting it WILL break the code.
         }
     }
 }
-function isArray(Arr){
-    if(typeof(Arr)!="object"){
+function isArray(Arr) {
+    if (typeof (Arr) != "object") {
         return false//its not even an object
     }
-    if("length" in Object.keys(Arr)){
+    if ("length" in Object.keys(Arr)) {
         return true
     }
     return false
 }
-function mkGrid(){//!!VERY IMPORTANT
-    aGrid.innerHTML=""
-    dGrid.innerHTML=""//clears the grids, in case theres anything in them.
-    let rowing = 100/rows
-    let coling = 100/columns
-    for(let y=1;y<=rows;y++){//creates all the divs.
-        aGrid.style.gridTemplateRows+=rowing+"% "
-        dGrid.style.gridTemplateRows+=rowing+"% "//also sets the grid templates correctly
-        for(let x=1;x<=columns;x++){
-            if(y==1){
-                aGrid.style.gridTemplateColumns+=coling+"% "
-                dGrid.style.gridTemplateColumns+=coling+"% "
+function mkGrid() {//!!VERY IMPORTANT
+    aGrid.innerHTML = ""
+    dGrid.innerHTML = ""//clears the grids, in case theres anything in them.
+    let rowing = 100 / rows
+    let coling = 100 / columns
+    for (let y = 1; y <= rows; y++) {//creates all the divs.
+        aGrid.style.gridTemplateRows += rowing + "% "
+        dGrid.style.gridTemplateRows += rowing + "% "//also sets the grid templates correctly
+        for (let x = 1; x <= columns; x++) {
+            if (y == 1) {
+                aGrid.style.gridTemplateColumns += coling + "% "
+                dGrid.style.gridTemplateColumns += coling + "% "
             }
-            dGrid.innerHTML+="<div id='d"+x+","+y+"'></div>"
-            aGrid.innerHTML+="<div id='"+x+","+y+"'></div>"
+            dGrid.innerHTML += "<div id='d" + x + "," + y + "'></div>"
+            aGrid.innerHTML += "<div id='" + x + "," + y + "'></div>"
         }
     }
-    for(let i=1;i<=columns;i++){//the colums and rows have to be created in the reverse order, so we can't do this while making the divs.
-        gridObj[i]={}
+    for (let i = 1; i <= columns; i++) {//the colums and rows have to be created in the reverse order, so we can't do this while making the divs.
+        gridObj[i] = {}
     }
-    for(x in gridObj){
-        for(let i=1;i<=rows;i++){
-            gridObj[x][i]={
-                "dis":document.getElementById("d"+x+","+i),
-                'act':document.getElementById(x+","+i),
-                'content':"",//should either be a string or an enemy object or an item object
-                'loc':x+","+i,//so that the corrdinate is easily accessible
+    for (x in gridObj) {
+        for (let i = 1; i <= rows; i++) {
+            gridObj[x][i] = {
+                "dis": document.getElementById("d" + x + "," + i),
+                'act': document.getElementById(x + "," + i),
+                'content': "",//should either be a string or an enemy object or an item object
+                'loc': x + "," + i,//so that the corrdinate is easily accessible
             }
         }
     }
 }
-function setStart(){
-    do{
-        var href=getCorrdHref(ranArrCorrd())
-    }while(href.content)//causes the do statement to repeat if the tile has something on it already.
-    href.dis.style.backgroundColor="green";
-    playerLoc=href.loc;
+function setStart() {
+    do {
+        var href = getCorrdHref(ranArrCorrd())
+    } while (href.content)//causes the do statement to repeat if the tile has something on it already.
+    href.dis.style.backgroundColor = "green";
+    playerLoc = href.loc;
 }
-function setExit(){
-do{
-    var href = getCorrdHref(ranArrCorrd())
-}while(href.dis.style.backgroundColor||cardAdj(playerLoc).indexOf(href.loc)!=-1)//TODO: get rid of the latter part of this and have the pathfinder make sure they aren't too close.
-//href.dis.style will be false if nothing is there, which should accord if the background color isn't set.
-//playerLoc is being used as a standin for the start point, since they should be the same at this time
-    href.dis.style.backgroundColor="red";
+function setExit() {
+    do {
+        var href = getCorrdHref(ranArrCorrd())
+    } while (href.dis.style.backgroundColor || cardAdj(playerLoc).indexOf(href.loc) != -1)//TODO: get rid of the latter part of this and have the pathfinder make sure they aren't too close.
+    //href.dis.style will be false if nothing is there, which should accord if the background color isn't set.
+    //playerLoc is being used as a standin for the start point, since they should be the same at this time
+    href.dis.style.backgroundColor = "red";
 }
 function cardAdj(corra) {//gets corrdinates that are cardinally adjecent to a given point.
-var temp = [];
-for (let i = 37; i <= 40; i++) {//use this for state to get all corrdinates cardinally adjecent to a point.
-    temp.push(switchMovement(corra, i));
+    var temp = [];
+    for (let i = 37; i <= 40; i++) {//use this for state to get all corrdinates cardinally adjecent to a point.
+        temp.push(switchMovement(corra, i));
+    }
+    return temp;//returns array
 }
-return temp;//returns array
-}
-function getCorrdHref(cor){
-    if(typeof(cor)=="string"){//we were given a string corrdinate, but need an array to do this properly.
-        cor=cor.split(",");
+function getCorrdHref(cor) {
+    if (typeof (cor) == "string") {//we were given a string corrdinate, but need an array to do this properly.
+        cor = cor.split(",");
     }
     return gridObj[cor[0]][cor[1]];//this line can error if the player attempts to move out of bounds, which is fine.
 }
-function interact(){
+function interact() {
     var href = getCorrdHref(playerLoc);
     //code for getting treasure goes here.
-    if(href.dis.style.backgroundColor=="red"){//is the background of the current tile red? (AKA, is it the exit?)
+    if (href.dis.style.backgroundColor == "red") {//is the background of the current tile red? (AKA, is it the exit?)
         nextLevel()
     }
 }
@@ -111,20 +111,20 @@ function keyPress() {
         interact();
         return;//we don't need to do math on the location.
     }
-    try{
-    var href = getCorrdHref(switchMovement(playerLoc, key))//this line errors if the player attempts to move to a location outside of the grid, which is fine.
-    }catch(err){
-        if(err=TypeError){
+    try {
+        var href = getCorrdHref(switchMovement(playerLoc, key))//this line errors if the player attempts to move to a location outside of the grid, which is fine.
+    } catch (err) {
+        if (err = TypeError) {
             throw "Player likely attempted to move to an out of bounds location, if that is the case such this error is intentional."
         } else {
             throw err.message
         }
     }
-    while(href.content) {
-        if(href.content.health){//does the tile contain an enemy?
+    while (href.content) {
+        if (href.content.health) {//does the tile contain an enemy?
             //yes it does
             //INSERT ATTACKING ENEMY HERE.
-        } else if(typeof(href.content)=="object"){//if its not an enemy, but it is an object it must be a(n) item/treasure.
+        } else if (typeof (href.content) == "object") {//if its not an enemy, but it is an object it must be a(n) item/treasure.
             break;//exits the while loop so we can still move.
         }
         //if the tile contains something, but it isn't either an enemy or an item
@@ -133,7 +133,7 @@ function keyPress() {
     playerLoc = href.loc;
     resetLocDisplay()
 }
-function clearLocDisplay(){
+function clearLocDisplay() {
     for (y = 1; y <= rows; y++) {
         for (x = 1; x <= columns; x++) {
             document.getElementById(x + "," + y).innerHTML = "";
@@ -146,54 +146,54 @@ function resetLocDisplay() {
     document.getElementById(playerLoc).innerHTML = "X";
 }
 function switchMovement(Corrd, key) {//movement function, returns a string
-let temp;//incase the result becomes invalid, we need to return the orginial input.
-if (typeof (Corrd) == "string") {
-    Corrd = Corrd.split(",");
-} else if (typeof (Corrd) != "object") {//this else if is mostly for debugging purposes.
-    throw "invalid parameter for Corrd.";
-}
+    let temp;//incase the result becomes invalid, we need to return the orginial input.
+    if (typeof (Corrd) == "string") {
+        Corrd = Corrd.split(",");
+    } else if (typeof (Corrd) != "object") {//this else if is mostly for debugging purposes.
+        throw "invalid parameter for Corrd.";
+    }
     temp = Corrd;
-switch (key) {//!!DO NOT EDIT, THIS WORKS CORRECTLY; copying the equations is recommended, however.
-    case 40:
-        temp[1] = Number(temp[1]) + 1;//down
-        break;
-    case 39://the Number() in the first 2 cases is so that 1+1!=11
-        temp[0] = Number(temp[0]) + 1;//right
-        break;
-    case 38:
-        temp[1] = temp[1] - 1;//up
-        break;
-    case 37://javascript can only do subtraction with numbers, so it will auto cor
-        temp[0] = temp[0] - 1;//left
-        break;
-    default:
-        //invalid input for key
-        return Corrd//return orginial corrdinate
-    //just have it return corrd so it doesn't break.
-}
+    switch (key) {//!!DO NOT EDIT, THIS WORKS CORRECTLY; copying the equations is recommended, however.
+        case 40:
+            temp[1] = Number(temp[1]) + 1;//down
+            break;
+        case 39://the Number() in the first 2 cases is so that 1+1!=11
+            temp[0] = Number(temp[0]) + 1;//right
+            break;
+        case 38:
+            temp[1] = temp[1] - 1;//up
+            break;
+        case 37://javascript can only do subtraction with numbers, so it will auto cor
+            temp[0] = temp[0] - 1;//left
+            break;
+        default:
+            //invalid input for key
+            return Corrd//return orginial corrdinate
+        //just have it return corrd so it doesn't break.
+    }
     return temp//returns new corrdinate as array
 }
 //mk start, end, and walls functions, also the random corrdinate function.
-function ranArrCorrd(){//returns array,
-    return [Math.floor(Math.random()*columns)+1,Math.floor(Math.random()*rows)+1]
+function ranArrCorrd() {//returns array,
+    return [Math.floor(Math.random() * columns) + 1, Math.floor(Math.random() * rows) + 1]
 }
-function ranStrCorrd(){
-    return Math.floor(Math.random()*columns)+1+","+Math.floor(Math.random()*rows)+1;
+function ranStrCorrd() {
+    return Math.floor(Math.random() * columns) + 1 + "," + Math.floor(Math.random() * rows) + 1;
 }
-function mkWalls(){
-    var hold=[];
-    do{
-        let temp=ranArrCorrd();//its an array for now.
-        if(hold.indexOf(temp)!=-1){
+function mkWalls() {
+    var hold = [];
+    do {
+        let temp = ranArrCorrd();//its an array for now.
+        if (hold.indexOf(temp) != -1) {
             continue;
         } else {
             hold.push(temp);
         }
     }
-    while(hold.length<10)
-        for(corrd of hold){
-            let href=gridObj[corrd[0]][corrd[1]]
-            href.dis.style.backgroundColor="black"
-            href.content="wall";
-        }
+    while (hold.length < 10)
+    for (corrd of hold) {
+        let href = gridObj[corrd[0]][corrd[1]]
+        href.dis.style.backgroundColor = "black"
+        href.content = "wall";
+    }
 }
