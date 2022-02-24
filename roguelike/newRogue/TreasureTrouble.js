5d6cd1bdaf5dc65da9e5d16e8f69615b59f04247
redguy999 <Bigredguy05@hotmail.com>
2022-02-24 08:50:52 -0500

    Organizes stuff better.

---
+++ roguelike/newRogue/TreasureTrouble.js
@@ -0,0 +1,56 @@
+function pickEnemy() {
+    let hold = [];
+    for (x in enemies) {
+        //this will need modified for the floors
+        hold.push(x);
+    }
+    let temp = Math.floor(Math.random() * hold.length); //pick a number equal to or between 0 and the highed index number of the list.
+    return hold[temp]; //returns the randomly picked enemy (specificly the name)
+}
+function placeEnemies() {
+    var href = getCorrdHref(ranArrCorrd());
+    let name = pickEnemy();
+    let eHold = enemies[name];
+    href.dis.style.backgroundColor = eHold.color;
+    href.content = new enemy(name, eHold.hp, eHold.atk, eHold.def, href);
+    //href.content=new
+}
+function emptyTile(loc) {
+    //might need to be made async so it doesn't bug the hell out of the program.
+    loc.content = "";
+    loc.dis.style.backgroundColor="";
+}
+//above is enemy code, the rest is below
+function mkOtherStuff() {
+    //placeEnemies();
+    placeItems();
+}
+function placeItems() {//!!TODO: update this to the version the stable is using.
+    var href;
+    var hold = Math.floor(Math.random()+1)*3
+    for(i=0;i<hold;i++){
+        do{
+            href = getCorrdHref(ranArrCorrd());
+        }
+        while(href.content || href.dis.style.backgroundColor)
+        href.content = new treasure(GIFL(),2,href)//TODO: replace 2 with the actual amount function.
+        href.dis.style.backgroundColor = "gold"
+        console.log(href.content)
+    }
+}
+class treasure{
+    constructor(cont,amount,loc){
+        this.contents = cont;//string
+        this.amount = amount;
+        this.location = loc;//grid object ref
+        this.getItem = function(){
+            if(this.amount==1){
+                alert("you found a "+this.contents+".")
+            }else{
+                alert("you found some "+this.contents+"s.");//change this to log at the bottom console
+            }
+            addToInventory(this.contents,this.amount);  
+            emptyTile(this.location)//JS is fine with this.
+        }
+    }
+}
---
+++ roguelike/newRogue/player.js
@@ -0,0 +1,76 @@
+const inventory = {//player's inventory.
+
+}
+const player = {//could probably move the location variable here but that would be kinda annoying.
+    attack: 1,//default values, attack should never go below 1 while alive.
+    defense: 0,//TODO: make this actually do stuff.
+    Mhealth: 100,
+    health: 100,
+    special: null,//null means none
+    hurtPlayer: function (damg) {//this could be its own function but its easier to call the variables here.
+        //defense and special attribute checks go here.
+        //debugger;
+        let temp = 0;
+        if (damg > 0) {
+            temp = Math.ceil(damg - (this.defense / 2))//current defense calucation formula, will likely change it.
+            if (temp <= 0) {
+                TtC("Your armor completely protects you from harm!")
+                updateHealth()
+                return;//no damage to deal, skip to end.
+            }
+        } else {//if damage is negative, ignore defense.
+            temp = damg
+        }
+        this.health -= temp;
+        if (temp > 0) {
+            TtC("You take " + temp + " damage!");
+        } else {
+            TtC("You heal " + (-temp) + " health.");//TODO: reword this.
+        }
+        if (this.health > this.Mhealth) {//this function can be used for healing, so we need to check for this edge case.
+            this.health = this.Mhealth;
+        } else if (this.health <= 0) {
+            playerDead();
+            return;
+        }
+        updateHealth()
+    },
+    updateStats: function () {
+        this.attack = 1;
+        this.defense = 0;
+        this.Mhealth = 100;//default values
+        this.special = null;//we have no way of editing this currently but i'll leave it here.
+        for (x in equipment) {
+            for (y in equipment[x]) {//reads every property of every slot
+                if (equipment[x][y] == "empty") {//there's nothing here.
+                    continue;//TODO:
+                }
+                this[y] += equipment[x][y];
+            }
+        }
+        for (x in player) {//sanity check
+            if (typeof (x) == "undefined") {
+                console.error("stat became invalid, printing stat logs:");
+                console.log(this.attack);
+                console.log(this.defense);
+                console.log(this.Mhealth);
+                console.log("reseting values to default:")
+                this.attack = 1;
+                this.defense = 0;
+                this.Mhealth = 100;
+                this.special = null;
+            }
+        }
+        if (this.health > this.Mhealth) {
+            this.health = this.Mhealth;
+        }
+        updateStatistics();
+    },
+    forceKill: function () {
+        this.health=0;
+        this.death();
+    },
+    death:function(){
+        //insert playerDead() from the old code here.
+    }
+}
