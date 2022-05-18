c5b49bf2803d3267f3fec4d7013086b3297357e9
redguy999 <Bigredguy05@hotmail.com>
2022-05-18 08:15:16 -0400

    Makes canvas actually able to be drawn on.

--- roguelike/newRogue/canvas.js
+++ roguelike/newRogue/canvas.js
@@ -1,2 +1,2 @@
-const canvas=document.getElementsByName("canvas")
-var artBoard = canvas.getContext()
+const canvas=document.getElementsByClassName("gScreen")[1]//get element by name didn't work so i'm trying this.
+var artBoard = canvas.getContext("2d")
